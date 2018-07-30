15b7367d5d292a4f843f3f3aa683b9885d686da7
Ethan <Ethan.Ji@nielsen.com>
2018-07-30 16:32:05 -0400

    made some changes on the time series model

--- forecast_store_item.py
+++ forecast_store_item.py
@@ -10,36 +10,12 @@ import numpy as np
 import matplotlib.pylab as plt
 from matplotlib.pylab import rcParams
 rcParams['figure.figsize'] = 15, 6
-
-#############################################################################
-#Build a sef defined function to check time series' stationary
 from statsmodels.tsa.stattools import adfuller
-def test_stationarity(timeseries):
-    
-    #Determing rolling statistics
-    rolmean = pd.rolling_mean(timeseries, window=12)
-    rolstd = pd.rolling_std(timeseries, window=12)
 
-    #Plot rolling statistics:
-    orig = plt.plot(timeseries, color="blue",label="Original")
-    mean = plt.plot(rolmean, color="red", label="Rolling Mean")
-    std = plt.plot(rolstd, color="black", label = "Rolling Std")
-    plt.legend(loc="best")
-    plt.title("Rolling Mean & Standard Deviation")
-    plt.show(block=False)
-    
-    #Perform Dickey-Fuller test:
-    print ("Results of Dickey-Fuller Test:")
-    dftest = adfuller(timeseries, autolag="AIC")
-    dfoutput = pd.Series(dftest[0:4], index=["Test Statistic","p-value","#Lags Used","Number of Observations Used"])
-    for key,value in dftest[4].items():
-        dfoutput["Critical Value (%s)"%key] = value
-    print (dfoutput)
-#End of the self defined function
-#############################################################################
-
+#read in .csv data file
 data_train = pd.read_csv("train.csv", parse_dates = ["date"], index_col = "date")
 
+#explore this data set
 print (data_train.head())
 print ("\n Data Types:")
 print (data_train.dtypes)
@@ -48,11 +24,32 @@ data_train.index
 m = max(data_train["store"])
 n = max(data_train["item"])
 k = data_train.shape[0]
-l = int(k/(m*n))
+p = int(k/(m*n))
 
-ts_train = data_train.iloc[0:l, 2]
+#slice by different store and item
+ts_train = data_train.iloc[0:p, 2]
 
-plt.plot(ts_train)
+#Determing rolling statistics
+rolmean = pd.Series.rolling(ts_train, window=12).mean()
+rolstd = pd.Series.rolling(ts_train, window=12).std()
 
-test_stationarity(ts_train)
+#Plot rolling statistics:
+orig = plt.plot(ts_train, color="blue",label="Original")
+mean = plt.plot(rolmean, color="red", label="Rolling Mean")
+std = plt.plot(rolstd, color="black", label = "Rolling Std")
+plt.legend(loc="best")
+plt.title("Rolling Mean & Standard Deviation")
+plt.show(block=False)
+
+#Perform Dickey-Fuller test:
+print ("Results of Dickey-Fuller Test:")
+dftest = adfuller(ts_train, autolag="AIC")
+dfoutput = pd.Series(dftest[0:4], index=["Test Statistic","p-value","#Lags Used","Number of Observations Used"])
+for key,value in dftest[4].items():
+    dfoutput["Critical Value (%s)"%key] = value
+print (dfoutput)
+
+if dfoutput["p-value"] > 0.01:
+      
+
     
